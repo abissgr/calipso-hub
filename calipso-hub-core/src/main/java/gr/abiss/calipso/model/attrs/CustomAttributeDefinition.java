7bba4a2434165f24994e2d679b8e9d8bc713a3c9
manosbatsis <manos@abiss.gr>
2013-07-27 18:43:30 +0300

    added missing fiules

---
+++ calipso-hub-core/src/main/java/gr/abiss/calipso/model/attrs/CustomAttributeDefinition.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2007 - 2013 www.Abiss.gr
+ *
+ * This file is part of Calipso, a software platform by www.Abiss.gr.
+ *
+ * Calipso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Calipso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Calipso. If not, see http://www.gnu.org/licenses/agpl.html
+ */
+package gr.abiss.calipso.model.attrs;
+
+
+import gr.abiss.calipso.model.base.AbstractAuditable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "custom_attribute")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class CustomAttributeDefinition extends
+		AbstractAuditable {
+
+	private static final long serialVersionUID = 4008152148196369725L;
+
+	@Column(name = "name", nullable = false, updatable = false)
+	private String name;
+
+	@Column(name = "className", nullable = false, updatable = false)
+	private String className;
+
+	@ManyToOne
+	@JoinColumn(name = "subject", referencedColumnName = "id", nullable = false)
+	private BaseCustomAttributesSchema subject;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BaseCustomAttributesSchema getSubject() {
+		return subject;
+	}
+
+	public void setSubject(BaseCustomAttributesSchema subject) {
+		this.subject = subject;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+}
