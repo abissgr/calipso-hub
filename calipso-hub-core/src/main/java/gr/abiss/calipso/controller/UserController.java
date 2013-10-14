f2bc84be34b3dd4ad980c6b0ab884d7f0dd210a5
manosbatsis <manos@abiss.gr>
2013-10-14 09:46:10 +0300

    fixed user controller mapping

--- calipso-hub-core/src/main/java/gr/abiss/calipso/controller/UserController.java
+++ calipso-hub-core/src/main/java/gr/abiss/calipso/controller/UserController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping(value = "/api/user", produces = { "application/json", "application/xml" })
+@RequestMapping(value = "/user", produces = { "application/json", "application/xml" })
 public class UserController extends AbstractServiceBasedRestController<User, String, UserService> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
