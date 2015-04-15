21d10445fa18631e82de9eb1489b3368ccc288d8
manosbatsis <manos@abiss.gr>
2015-04-15 13:37:01 +0300

    refactored auditable resources

--- calipso-hub-framework/src/main/java/gr/abiss/calipso/model/base/AbstractCategory.java
+++ calipso-hub-framework/src/main/java/gr/abiss/calipso/model/base/AbstractCategory.java
@@ -23,45 +23,18 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * Resource categories are hierarchical, have aliases and can be used as tags
  */
 @MappedSuperclass
-public abstract class AbstractCategory<T extends AbstractCategory<T>> extends AuditableResource {
+public abstract class AbstractCategory<T extends AbstractCategory<T>> extends AuditableResource<T> {
 
 	private static final long serialVersionUID = -1329254539598110186L;
 
-	@JsonIgnore
-	@ManyToOne(/* cascade=CascadeType.ALL, */fetch = FetchType.EAGER)
-	@JoinColumn(name = "same_as", referencedColumnName = "id", nullable = true)
-	private T sameAs;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "parent", referencedColumnName = "id", nullable = true)
-	private T parent;
-
-	@JsonIgnore
-	@OneToMany(mappedBy = "parent", /* cascade=CascadeType.ALL, */ fetch=FetchType.LAZY)
-	private List<T> children = new ArrayList<T>(0);
-	
 	public AbstractCategory(String name) {
-		this.setName(name);
+		super(name);
+	}
+
+	public AbstractCategory(String name, T parent) {
+		super(name, parent);
 	}
 	
-	public AbstractCategory(String name, T parent) {
-		this(name);
-		this.setParent(parent);
-	}
-	@PreUpdate
-    @PrePersist
-    public void normalizePath() throws UnsupportedEncodingException{
-		if(this.getPath() == null){
-			StringBuffer path = new StringBuffer();
-			if(this.getParent() != null){
-				path.append(this.getParent().getPath());
-			}
-			path.append('/');
-			path.append(URLEncoder.encode(this.getName(), "UTF-8"));
-			this.setPath(path.toString());
-		
-		}
-	}
 	
 	@Override
     public boolean equals(Object obj) {
@@ -77,30 +50,6 @@ public abstract class AbstractCategory<T extends AbstractCategory<T>> extends Au
         eb.append(this.getPath(), that.getPath());
         return eb.isEquals();
     }
-
-	public T getSameAs() {
-		return sameAs;
-	}
-
-	public void setSameAs(T sameAs) {
-		this.sameAs = sameAs;
-	}
-
-	public T getParent() {
-		return parent;
-	}
-
-	public void setParent(T parent) {
-		this.parent = parent;
-	}
-
-	public List<T> getChildren() {
-		return children;
-	}
-
-	public void setChildren(List<T> children) {
-		this.children = children;
-	}
 
 	
 }
--- calipso-hub-framework/src/main/java/gr/abiss/calipso/model/base/AuditableResource.java
+++ calipso-hub-framework/src/main/java/gr/abiss/calipso/model/base/AuditableResource.java
@@ -1,32 +1,91 @@
 package gr.abiss.calipso.model.base;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import gr.abiss.calipso.model.User;
 import gr.abiss.calipso.model.entities.AbstractAuditable;
 
 import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 /**
- * A base class for auditable resource entities
+ * A base class for auditable resource entities: files, folders, categories etc.
  */
-
 @MappedSuperclass
-public abstract class AuditableResource extends AbstractAuditable<User> {
+public abstract class AuditableResource<T extends AuditableResource<T>> extends AbstractAuditable<User> {
 
 	/**
 	 * The HTTP URL of the resource, excluding the protocol, domain and port. Starts with a slash. 
 	 */
 	@Column(name = "name", nullable = false)
 	private String name;
-	
+
 	/**
 	 * The HTTP URL of the resource, excluding the protocol, domain and port. Starts with a slash. 
 	 */
 	@Column(name = "path", nullable = false)
 	private String path;
+	
+	/**
+	 * The HTTP URL of the resource, excluding the protocol, domain and port. Starts with a slash. 
+	 */
+	@Column(name = "path_level", nullable = false)
+	private Short pathLevel;
+
+	@JsonIgnore
+	@ManyToOne(/* cascade=CascadeType.ALL, */fetch = FetchType.EAGER)
+	@JoinColumn(name = "same_as", referencedColumnName = "id", nullable = true)
+	private T sameAs;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent", referencedColumnName = "id", nullable = true)
+	private T parent;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "parent", /* cascade=CascadeType.ALL, */ fetch=FetchType.LAZY)
+	private List<T> children = new ArrayList<T>(0);
+	
+	public AuditableResource(String name) {
+		this.setName(name);
+	}
+	public AuditableResource(String name, T parent) {
+		this(name);
+		this.setParent(parent);
+	}
+	
+	@PreUpdate
+    @PrePersist
+    public void normalizePath() throws UnsupportedEncodingException{
+		// set path
+		if(this.getPath() == null){
+			StringBuffer path = new StringBuffer();
+			if(this.getParent() != null){
+				path.append(this.getParent().getPath());
+			}
+			path.append('/');
+			path.append(URLEncoder.encode(this.getName(), "UTF-8"));
+			this.setPath(path.toString());
+		}
+		// set path level
+		Integer pathLevel = StringUtils.countMatches(this.getPath(), "/");
+		this.setPathLevel(pathLevel.shortValue());
+		
+	}
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -47,22 +106,43 @@ public abstract class AuditableResource extends AbstractAuditable<User> {
 		builder.append(this.getPath(), other.getPath());
 		return builder.isEquals();
 	}
-
+	
+	
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getPath() {
 		return path;
 	}
-
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
-	
+	public Short getPathLevel() {
+		return pathLevel;
+	}
+	public void setPathLevel(Short pathLevel) {
+		this.pathLevel = pathLevel;
+	}
+	public T getSameAs() {
+		return sameAs;
+	}
+	public void setSameAs(T sameAs) {
+		this.sameAs = sameAs;
+	}
+	public T getParent() {
+		return parent;
+	}
+	public void setParent(T parent) {
+		this.parent = parent;
+	}
+	public List<T> getChildren() {
+		return children;
+	}
+	public void setChildren(List<T> children) {
+		this.children = children;
+	}
+
 }
