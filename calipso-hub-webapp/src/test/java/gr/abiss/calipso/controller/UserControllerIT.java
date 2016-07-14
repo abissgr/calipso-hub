91410ec374fa64bd546e277fc6def74810bda4e5
manos <manos@sobol>
2016-07-14 04:26:56 +0300

    added user IT

---
+++ calipso-hub-webapp/src/test/java/gr/abiss/calipso/controller/UserControllerIT.java
@@ -0,0 +1,48 @@
+/**
+ * calipso-hub-webapp - A full stack, high level framework for lazy application hackers.
+ * Copyright © 2005 Manos Batsis (manosbatsis gmail)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gr.abiss.calipso.controller;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import gr.abiss.calipso.test.AbstractControllerIT;
+import gr.abiss.calipso.userDetails.model.LoginSubmission;
+import gr.abiss.calipso.utils.Constants;
+import io.restassured.response.Response;
+
+@Test(/*singleThreaded = true, */description = "Test dynamic JPA specifications used in default search stack")
+@SuppressWarnings("unused")
+public class UserControllerIT extends AbstractControllerIT {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserControllerIT.class);
+
+	@Test(description = "Test logging in with correct credentials")
+	public void testCorrectLogin() throws Exception {
+		this.getLoggedinContext("admin", "admin");
+	}
+
+}
